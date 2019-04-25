06e60edbb4b0268774b2be758b9c07b81255e5b9
WaelSan <waelb.insat@gmail.com>
2019-04-25 15:41:07 +0300

    Update 01_installallnodes.sh
    Edit for Ubuntu server 18

--- 01_installallnodes.sh
+++ 01_installallnodes.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
+# Create postgres user in each server
 useradd postgres --shell /bin/bash --home /home/postgres --create-home
 echo -e 'postgres\npostgres\n' | sudo passwd postgres
+# Add postgres user to sudo group
 adduser postgres sudo
 chsh -s /bin/bash postgres
+# Install Dependencies
 apt-get update -y
-apt-get install software-properties-common python-software-properties -y 
+sudo apt --fix-broken install python-pycurl python-apt -y
+apt-get install software-properties-common -y 
 apt-get install wget zip gcc libreadline6 libreadline6-dev zlib1g-dev build-essential flex -y
+# Clone postgres-xl
 cd /home/postgres
 wget https://sourceforge.net/projects/postgres-xl/files/latest/download
 
